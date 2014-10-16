2e7d3d7fd19c1a8a3a0135d98b372e3d8635840d
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-10-15 22:44:22 -0500

    wip: map PDL types to R

--- lib/R/DataConvert/PDL.pm
+++ lib/R/DataConvert/PDL.pm
@@ -2,7 +2,28 @@ package R::DataConvert::PDL;
 
 use strict;
 use warnings;
+use Inline 'Pdlpp';
+
+my $charsxp =  { sexptype => 'CHARSXP' };
+my $intsxp = { sexptype => 'INTSXP' };
+my $realxsp = { sexptype => 'REALSXP' };
+my $pdl_to_r = {
+		PDL_B   => $charsxp,
+
+		PDL_S   => $intsxp,
+		PDL_US  => $intsxp,
+		PDL_L   => $intsxp,
+		PDL_IND => $intsxp,
+		PDL_LL  => $intsxp,
+
+		PDL_F   => $realxsp,
+		PDL_D   => $realxsp,
+};
 
 # TODO
 
 1;
+__DATA__
+__Pdlpp__
+
+
