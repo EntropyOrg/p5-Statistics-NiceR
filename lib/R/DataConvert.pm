a02eae2eddd92270198996589561c0db30c1e808
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-10-18 15:42:13 -0500

    multiple converters

--- lib/R/DataConvert.pm
+++ lib/R/DataConvert.pm
@@ -5,14 +5,29 @@ use warnings;
 
 use R::Sexp;
 use R::DataConvert::PDL;
-
+use R::DataConvert::Perl;
 
 sub convert_r_to_perl {
-	&R::DataConvert::PDL::convert_r_to_perl;
+	for my $p (qw(R::DataConvert::PDL R::DataConvert::Perl) ) {
+		my $ret;
+		eval {
+			no strict 'refs';
+			$ret = &{"${p}::convert_r_to_perl"}(@_);
+		};
+		return $ret if defined $ret;
+	}
+	die "could not convert";
 }
 
 sub convert_perl_to_r {
-	&R::DataConvert::PDL::convert_perl_to_r;
+	for my $p (qw(R::DataConvert::PDL R::DataConvert::Perl) ) {
+		my $ret;
+		eval {
+			no strict 'refs';
+			$ret = &{"${p}::convert_perl_to_r"}(@_);
+		};
+		return $ret if defined $ret;
+	}
 }
 
 
