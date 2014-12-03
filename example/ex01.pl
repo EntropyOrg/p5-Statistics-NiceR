865c905c066853d36ec25704b5b741deb08a8441
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-12-02 19:51:28 -0600

    change ex01.pl to use the R AUTOLOAD feature

--- example/ex01.pl
+++ example/ex01.pl
@@ -5,11 +5,10 @@ use strict;
 use warnings;
 
 use R;
-use R::DataConvert;
 use Data::Frame::Rlike;
 
-my $iris_r = Rinterp::eval_SV( q{ iris });
-my $iris = R::DataConvert->convert_r_to_perl( $iris_r );
+my $r = R->new;
+my $iris = $r->get('iris');
 say $iris->subset( sub {
 		  ( $_->('Sepal.Length') > 6.0 )
 		& ( $_->('Petal.Width')  < 2   )
