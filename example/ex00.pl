7001cbf2df4f35a814c90a8ebb5e8fc3750ec083
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-12-08 03:28:57 -0600

    use eval_parse in an example

--- example/ex00.pl
+++ example/ex00.pl
@@ -5,9 +5,10 @@ use warnings;
 
 use R;
 
-Rinterp::eval_SV( q{
-			library(aplpack)
-			faces(iris[1:20,1:4])
-		});
+my $r = R->new;
+my $face_data = $r->eval_parse(q{
+	library(aplpack)
+	faces(iris[1:20,1:4])
+	});
 
 <STDIN>;
