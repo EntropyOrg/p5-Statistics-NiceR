4f5a59d9b7f26e4370cdc25cd397e914d0f478dd
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-12-18 15:00:35 -0600

    R AUTOLOAD function name mapping
    Map `_` to `.` and `__` to `_`.
    
    Closes <https://github.com/zmughal/p5-Statistics-NiceR/issues/16>.

--- lib/Statistics/NiceR.pm
+++ lib/Statistics/NiceR.pm
@@ -13,15 +13,24 @@ sub new {
 	my $obj = bless { converter => 'Statistics::NiceR::DataConvert', r_interpreter => 'Statistics::NiceR::Backend::EmbeddedR' }, $klass;
 
 	# add this function to R env so that eval'ing a string is easy via the function call interface
-	$obj->{r_interpreter}->eval( q{ eval_parse <- function(x) eval( parse( text = x ) ) } );
+	$obj->{r_interpreter}->eval( q{ eval.parse <- function(x) eval( parse( text = x ) ) } );
 
 	$obj;
+}
+
+sub _map_function_perl_to_r {
+	my ($name) = @_;
+	$name =~ s,__,*,;
+	$name =~ s,_,.,;
+	$name =~ s,\*,_,;
+	$name;
 }
 
 sub AUTOLOAD {
 	(my $fname = our $AUTOLOAD) =~ s/^@{[__PACKAGE__]}:://;
 	my ($self, @args) = @_;
-	my $function = $self->{r_interpreter}->R_get_function($fname);
+	my $r_fname = _map_function_perl_to_r($fname); 
+	my $function = $self->{r_interpreter}->R_get_function($r_fname);
 	my @r_args = map { [ $self->{converter}->convert_perl_to_r($_) ] } @args;
 	my $r_data = $self->{r_interpreter}->R_call_function( $function, \@r_args );
 	my $perl_data = eval {
