04913e8a34158376de1cc14028726d68b1d67584
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-12-23 15:20:25 -0600

    wip: docs

--- lib/Statistics/NiceR.pm
+++ lib/Statistics/NiceR.pm
@@ -12,21 +12,31 @@ use Try::Tiny;
 
 =method new
 
-  new()
+    new()
 
 Creates a new instance of a wrapper around the R interpreter.
 
-   use Statistics::NiceR
-   my $r = Statistics::NiceR->new();
+Example
+
+    use Statistics::NiceR
+
+    my $r = Statistics::NiceR->new();
 
 =cut
 =method eval_parse
 
-   $eval_result = eval_parse( Str $r_code )
+    eval_parse( Str $r_code )
 
 A convenience function that allows for evaluating arbitrary R code.
 
 The return value is the last line of the code in C<$r_code>.
+
+Example:
+
+    use Statistics::NiceR
+
+    my $r = Statistics::NiceR->new();
+    my $dataframe = $r->eval_parse( q< iris[1:20,1:4] > );
 
 =cut
 sub new {
@@ -71,10 +81,65 @@ sub AUTOLOAD {
 
 =head1 SYNOPSIS
 
-=head1 CALLING R FUNCTIONS
+    use Statistics::NiceR
 
+    my $r = Statistics::NiceR->new();
 
+    say $r->pnorm( [ 0 .. 3 ] )
+    # [0.5 0.84134475 0.97724987  0.9986501]
 
-=head1
+=head1 DESCRIPTION
+
+This module provides an interface to the R programming language for statistics
+by embedding the R interpreter using its C API. This allows direct access to
+R's functions and allows sending and receiving data efficiently.
+
+=head2 CONVERSION
+
+In order to give the module a hassle-free interface, there is a mechanism to
+convert Perl types[^1] to R types[^2] and vice-versa for the values sent to and
+from the R interpreter.
+
+Currently, the conversion is handled by the modules under the
+L<Statistics::NiceR::DataConvert> namespace. It is currently undocumented how
+to extend this to more types or how to change the default behaviour, but this
+will be addressed in future versions.
+
+[^1]: Such as strings, numbers, and arrays.
+
+[^2]: Such as integers, numerics, data frames, and matrices.
+
+=begin comment
+TODO change this wording when the Statistics::NiceR::DataConvert module is
+documented and when changing the converter behaviour is documented for
+Statistics::NiceR
+=end comment
+
+=head2 CALLING R FUNCTIONS
+
+R functions can be called 
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<The R Project for Statistical Computing|http://www.r-project.org/>
+
+=item Browse and download additional R packages: L<The Comprehensive R Archive Network|http://cran.r-project.org/>
+
+=item L<R Tutorial: R Introduction|http://www.r-tutor.com/r-introduction>
+
+=back
+
+For developers:
+
+=over 4
+
+=item L<Writing R Extensions|http://cran.r-project.org/doc/manuals/r-release/R-exts.html>
+
+=item L<Advanced R by Hadley Wickham: R’s C interface|http://adv-r.had.co.nz/C-interface.html>
+
+=back
+
 
 =cut
